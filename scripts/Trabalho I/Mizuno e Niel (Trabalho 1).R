dcca592b7041709d3c0dade158614ebfc3b8f414
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-08-25 12:12:05 -0300

    Acrescentei alguns graficos

--- scripts/Trabalho I/Mizuno e Niel (Trabalho 1).R
+++ scripts/Trabalho I/Mizuno e Niel (Trabalho 1).R
@@ -119,19 +119,81 @@ sjt.xtab(dados$sexo,dados$serasa,show.summary=F,var.labels = c("Sexo","Possui no
 #Fazendo teste do chi-quadrado
 m <- matrix(c(56,37,34,63),nrow=2,ncol=2)
 chisq.test(m,correct=F)
-ggplot(dados,aes(x=serasa))+geom_bar(aes(fill=sexo))+ylab("Frequ?ncia")+xlab("Possui nome no SERASA")
+ggplot(dados,aes(x=serasa))+geom_bar(aes(fill=sexo))+ylab("Frequência")+xlab("Possui nome no SERASA")
+
+#Fazendo grafico
+
+serasa_sexo <- table(dados$serasa,dados$sexo)
+
+# Nomeando colunas  e linhas (respectivamente)
+colnames(serasa_sexo) <- c("Feminino", "Masculino")
+rownames(serasa_sexo) <- c("Sim", "Não")
+
+# Escolhendo cores
+sexo_serasa_cor <- c("#54b0f7","#ff91f7")
+
+# Calculando as proporções
+serasa_sexo <- apply(serasa_sexo, 2, function(x){x/sum(x,na.rm=T)}) ; serasa_sexo
+
+par(mar = c(5, 4, 4, 8))
+
+barplot(serasa_sexo, col=sexo_serasa_cor, 
+        border="white", 
+        xlab="Sexo", 
+        ylim = c(0,1.19),
+        ylab = "Proporção",
+        main = "Nome no SERASA por sexo")
+legend(x = "topright", 
+       legend = c("Satisfeito", "Não satisfeito"), 
+       fill = sexo_serasa_cor, 
+       title = "Legenda",
+       xpd = TRUE,
+       inset = c(-0.40,0.30))
+
+par(mar = c(5.1, 4.1, 4.1, 2.1))
 
 #Fazer descritivas do estado civil por sexo####
 sjt.xtab(dados$sexo,dados$estciv,show.summary=F,var.labels = c("Sexo","Possui nome no SERASA")
          ,show.row.prc = T)
 sjt.xtab(dados$sexo,dados$estciv,show.summary=F,var.labels = c("Sexo","Possui nome no SERASA")
          ,show.col.prc = T)
+
 #Fazendo teste do chi-quadrado
 m <- matrix(c(23,34,31,28,23,15,13,23),nrow=2,ncol=2)
 chisq.test(m,correct=F)
 
 ggplot(dados,aes(x=estciv))+geom_bar(aes(fill=sexo))+ylab("Frequ?ncia")+xlab("Estado Civil")
 
+#Fazendo grafico
+
+estciv_sexo <- table(dados$estciv,dados$sexo)
+
+# Nomeando colunas  e linhas (respectivamente)
+colnames(estciv_sexo) <- c("Feminino", "Masculino")
+rownames(estciv_sexo) <- c("Casado", "Solteiro","Viuvo","Outros")
+
+# Escolhendo cores
+estciv_sexo_cor <- c("#54b0f7","#ff91f7","red","green")
+
+# Calculando as proporções
+estciv_sexo <- apply(estciv_sexo, 2, function(x){x/sum(x,na.rm=T)}) ; estciv_sexo
+
+par(mar = c(5, 4, 4, 8))
+
+barplot(estciv_sexo, col=estciv_sexo_cor, 
+        border="white", 
+        xlab="Sexo", 
+        ylim = c(0,1.19),
+        ylab = "Proporção",
+        main = "Estado civil por sexo")
+legend(x = "topright", 
+       legend = c("Casado", "Solteiro","Viuvo","Outros"), 
+       fill = sexo_serasa_cor, 
+       title = "Legenda",
+       xpd = TRUE,
+       inset = c(-0.40,0.30))
+
+par(mar = c(5.1, 4.1, 4.1, 2.1))
 #Fazer descritivas da idade por satisfac?o#####
 
 #TH para normalidade de dados$satisflimite=="s"
@@ -156,37 +218,6 @@ var.test(dados$idade[dados$satisflimite=="s"],dados$idade[dados$satisflimite=="n
 t.test(dados$idade[dados$satisflimite=="s"],dados$idade[dados$satisflimite=="n"],var.equal=FALSE,
        alternative ="two.sided")
 
-#Fazendo grafico
-
-serasa_sexo <- table(dados$serasa,dados$sexo)
-
-# Nomeando colunas  e linhas (respectivamente)
-colnames(serasa_sexo) <- c("Feminino", "Masculino")
-rownames(serasa_sexo) <- c("Sim", "Não")
-
-# Escolhendo cores
-sexo_serasa_cor <- c("#54b0f7","#ff91f7")
-
-# Calculando as proporções
-serasa_sexo <- apply(serasa_sexo, 2, function(x){x/sum(x,na.rm=T)}) ; serasa_sexo
-
-par(mar = c(5, 4, 4, 8))
-
-barplot(serasa_sexo, col=sexo_serasa_cor, 
-        border="white", 
-        xlab="Sexo", 
-        ylim = c(0,1.19),
-        ylab = "Proporção",
-        main = "Gráfico de proporção do sexo \npor Satisfação")
-legend(x = "topright", 
-       legend = c("Satisfeito", "Não satisfeito"), 
-       fill = sexo_serasa_cor, 
-       title = "Legenda",
-       xpd = TRUE,
-       inset = c(-0.40,0.30))
-
-par(mar = c(5.1, 4.1, 4.1, 2.1))
-
 #Fazer descritivas do satisfacao por serasa####
 sjt.xtab(dados$satisflimite,dados$serasa,show.summary=F,var.labels = c("Satisfeito com limite?","Possui nome no SERASA")
          ,show.row.prc = T)
@@ -198,8 +229,6 @@ m <- matrix(c(23,70,28,69),nrow=2,ncol=2)
 chisq.test(m,correct=F)
 ggplot(dados,aes(x=satisflimite))+geom_bar(aes(fill=serasa))+ylab("Frequ?ncia")+
   xlab("Possui nome no SERASA")
-
-#TH para prop (COMO FAZER)
 
 #Fazer descritivas do satifacao por banco####
 sjt.xtab(dados$satisflimite,dados$banco,show.summary=F,var.labels = c("Satisfeito com limite?","Banco")
@@ -239,8 +268,6 @@ var.test(dados$tempocliente[dados$satisflimite=="n"],dados$tempocliente[dados$sa
 t.test(dados$tempocliente[dados$satisflimite=="n"],dados$tempocliente[dados$satisflimite=="s"],
        var.equal=TRUE,alternative ="two.sided")
 
-
-
 #Fazer descritivas do gerente por satisfa??o####
 sjt.xtab(dados$gerente,dados$satisflimite,show.summary=F,var.labels = c("Falou com gerente","Satisfeito com limite?")
          ,show.row.prc = T)
@@ -252,3 +279,32 @@ m <- matrix(c(23,28,79,60),nrow=2,ncol=2)
 chisq.test(m,correct=F)
 ggplot(dados,aes(x=satisflimite))+geom_bar(aes(fill=gerente))+ylab("Frequ?ncia")+
   xlab("Possui nome no SERASA")
+
+gerente_satis <- table(dados$gerente,dados$satisflimite)
+
+# Nomeando colunas  e linhas (respectivamente)
+colnames(gerente_satis) <- c("Sim", "Não")
+rownames(gerente_satis) <- c("Sim", "Não")
+
+# Escolhendo cores
+gerente_satis_cor <- c("#54b0f7","#ff91f7")
+
+# Calculando as proporções
+gerente_satis <- apply(gerente_satis, 2, function(x){x/sum(x,na.rm=T)}) ; gerente_satis
+
+par(mar = c(5, 4, 4, 8))
+
+barplot(gerente_satis, col=gerente_satis_cor, 
+        border="white", 
+        xlab="Sexo", 
+        ylim = c(0,1.19),
+        ylab = "Proporção",
+        main = "Falou com gerente por sexo")
+legend(list(x=2.3,y=0), 
+       legend = c("Usou gerente", "Não usou gerente"), 
+       fill = sexo_serasa_cor, 
+       title = "Legenda",
+       xpd = TRUE,
+       inset = c(-0.40,0.30))
+
+par(mar = c(5.1, 4.1, 4.1, 2.1))
