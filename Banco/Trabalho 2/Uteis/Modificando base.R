1f63ad2283169e31a4edfbca56d5a65e800d6973
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-11 16:20:56 -0300

    BOtei os codigos de cada estado no trab

---
+++ Banco/Trabalho 2/Uteis/Modificando base.R
@@ -0,0 +1,14 @@
+library(dplyr)
+base <- read_xlsx("AtlasBrasil_Consulta.xlsx")
+base <- mutate(base, Estado = substring(Código,1,2) )
+base <- base %>% select(Código,Espacialidades,Estado,everything())
+base$Estado[base$Estado==21]="MA"
+base$Estado[base$Estado==22]="PI"
+base$Estado[base$Estado==23]="CE"
+base$Estado[base$Estado==24]="RN"
+base$Estado[base$Estado==25]="PB"
+base$Estado[base$Estado==26]="PE"
+base$Estado[base$Estado==27]="AL"
+base$Estado[base$Estado==28]="SE"
+base$Estado[base$Estado==29]="BA"
+saveRDS(base, "Base_modificada.rds")
