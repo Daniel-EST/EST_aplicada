30f3fe151b6509c5c0c325c2c286095c4405f408
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-09-13 10:58:36 -0300

    mexemos no BD e decidimos como tratar as variaveis

--- Banco/Trabalho 2/Uteis/Matriz Covariancia e Correlação.R
+++ Banco/Trabalho 2/Uteis/Matriz Covariancia e Correlação.R
@@ -6,14 +6,18 @@ base <- base %>% select(-c(Espacialidades,Estado,Código,`População rural 2000
                            ,`População rural 2010`,`População rural 2000`
                            ,`População rural 2010`,`População urbana 2000`,
                            `População urbana 2010`))
-                           
-                          
 
+base$Estado <- base$Estado %>% 
+  factor()
+base$Espacialidades <- base$Espacialidades %>% 
+  factor()
+
+base <- base[-1,] # Tiramos a linha do Brasil
 m <- structure(base)
 mat_cor <- cor(m)
 mat_cov <- cov(m)
-
-summary(base)
-
+base$`asdfa asdfas asdf ad `
 write.csv(mat_cor,"Matriz Correlação.csv",row.names=FALSE)
 write.csv(mat_cov,"Matriz Covariancia.csv",row.names=FALSE)
+
+nomes <- names(base)
