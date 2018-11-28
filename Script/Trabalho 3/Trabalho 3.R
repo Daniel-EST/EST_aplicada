5b1b3c86d078c50e52a665888bed8b5ea8e89f1c
GMizuno <gabrielmizuno@gmail.com>
2018-11-28 15:43:41 -0200

    Fiz algumas analises

--- Script/Trabalho 3/Trabalho 3.R
+++ Script/Trabalho 3/Trabalho 3.R
@@ -1,7 +1,7 @@
 require(ggplot2); require(dplyr); library(DescTools); require(likert); require(magrittr)
 require(rgdal)
 #SO PARA O MIZUNO NÃO RODA ISSO, SE VC RODAR ISSO O PROBLEMA É SEU!!!!!!!!!
-Sys.setlocale("LC_ALL", "pt_BR.ISO8859-1")
+#Sys.setlocale("LC_ALL", "pt_BR.ISO8859-1")
 
 base <- readRDS("Banco/Trabalho 3/base1.rds")
 Likert <- readRDS("Banco/Trabalho 3/Likert.rds")
@@ -21,11 +21,12 @@ PostHocTest(comparacao, method = "bonferroni")
 #Estados
 #Acho melhor passa um summary e fazer um tabela
 summary(base$estado)
-base2= toupper(base$estado)
-base2 = as.factor(base$estado)
+
 base2 <- group_by(base, estado) %>% summarise(n = length(estado))
-mapa_br=readOGR("Banco/Trabalho 3/UFEBRASIL.shp",encoding ="UTF-8",use_iconv=TRUE, verbose=FALSE)
-mapa_br@data$NM_ESTADO = as.factor(mapa_br@data$NM_ESTADO)
+base2$estado <-  as.factor(base2$estado)
+base2$estado[base2$estado=="Espírito Santo"] <- "Espirito Santo"
+mapa_br <- readOGR("Banco/Trabalho 3/UFEBRASIL.shp",encoding ="UTF-8",use_iconv=TRUE, verbose=FALSE)
+mapa_br@data$NM_ESTADO <-  as.factor(mapa_br@data$NM_ESTADO)
 mapa_br@data <- inner_join(mapa_br@data,base2, by = c("NM_ESTADO" = "estado"))
 spplot(mapa_br, "n")
 
@@ -60,3 +61,13 @@ Likert %<>%  as.data.frame()
 tabela_likert <- likert(Likert)
 plot(tabela_likert)
 
+#Genero
+
+barplot(prop.table(table(base$sexo)),ylim = c(0, 1),main="Genero")
+
+#Requisitos minimos
+
+prop <- ifelse(base$requisitos=="Sim",1,0); mean(prop)
+prop.test(sum(prop), length(prop), alternative = "less", p = 0.5, correct = FALSE)
+
+barplot(prop.table(table(base$requisitos)),ylim = c(0, 1),main="Deixou de jogar por cuasa dos requisitos?")
