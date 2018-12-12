423ea72b89ea88a91035e57dc1c6a394fe149cab
nanda-uff <fernanda.fjuni@gmail.com>
2018-12-11 23:42:13 -0200

    script atualizado

--- Script/Trabalho 3/Trabalho 3.R
+++ Script/Trabalho 3/Trabalho 3.R
@@ -16,17 +16,27 @@ ggplot(base, aes(x = sexo, y = idade)) + geom_boxplot(fill="#00bfc4") + ylab("Id
 summary(base$estado)
 length(base$estado)
 
-#idade cat
-tb<-round(prop.table(table(base$idade.cat)),2)
+#graficos de pizza
+#basta trocar a variave em tb
+tb<-round(prop.table(table(base$civil)),2)
 dt<-as.data.frame(tb)
 
 pizza<-ggplot(dt, aes(x="",y=Freq, fill=Var1))+
   geom_bar(width = 1, stat="identity")+
   coord_polar("y")
 
-pizza+scale_fill_discrete(name="Idade Categórica")+theme_void()+ 
-  geom_text(aes(y = c(0.85,0.45,0.12),
+pizza+scale_fill_discrete(name="Estado Civil")+theme_void()+ 
+  geom_text(aes(y = c(0.83,0.3, 0.5,0.1),
                 label = percent(Freq,accuracy=1)), data = dt, size=4.6)+ylab("")+xlab("")
+
+a<-as.data.frame(round(prop.table(table(base$escolaridade)),3))
+
+#proporções
+round(prop.table(table(base$dinheiro))*100,1)
+5.2+2.6+1.3+0.7+0.3+0.3
+
+table(base$horas_semana)
+
 
 #Verificando Homocedacidade
 LeveneTest(base$idade, base$sexo, center = "mean")
@@ -104,17 +114,49 @@ tabela_likert <- likert(Likert)
 c <- c("#f2766d","#00bfc4","#7cae00") 
 plot(tabela_likert,color=c)+ ylab("Porcentagem")
 
-#Tipo de plataforma por genero jogo 
+#produtos por dinheiro
 
+base4<-select(base, c(Skins:Outros_p))
+base4<-apply(base4, 2, function(x)ifelse(x=="Sim",1,0 )) %>% as.data.frame()
+base$quant_pro<- apply(base4, 1, sum)     
+
+round(mean(base$quant_pro),2)
+
+base$quant_pro<-base$quant_pro%>%as.factor()
+base$quant_pro
+
+base %>%
+  ggplot(aes(x=dinheiro)) + 
+  geom_bar(aes(fill=quant_pro), position = "fill")+ylab("Frequência")+scale_fill_discrete(name="Produtos")+xlab("Já gastou mais dinheiro do que deveria?")
+
+summary(base$quant_pro)
+
+#dinheiro por genero que joga
+base$quant_jogos<-base$quant_jogos%>%as.factor()
+
+base %>%
+  ggplot(aes(x=dinheiro)) + 
+  geom_bar(aes(fill=quant_jogos), position = "dodge")+ylab("Frequência")+scale_fill_discrete(name="Gênero de Jogo")+xlab("Já gastou mais dinheiro do que deveria?")
+
+#dinheiro por plataforma
+base$quant_plant<-base$quant_plant%>%as.factor()
+
+base %>%
+  ggplot(aes(x=dinheiro)) + 
+  geom_bar(aes(fill=quant_plant), position = "dodge")+ylab("Frequência")+scale_fill_discrete(name="Plataforma")+xlab("Já gastou mais dinheiro do que deveria?")
+
+#Tipo de plataforma por genero jogo 
 base2 <- select(base,c(FPS:OUTROS))
 base2 <- apply(base2, 2, function(x)ifelse(x=="Sim",1,0 )) %>% as.data.frame()
-base2$quant_jogos <- apply(base2, 1, sum)
+base$quant_jogos <- apply(base2, 1, sum)
+
+mean(base$quant_jogos)
 
 base3 <- select(base,c(PC:Outros_pla))
 base3 <- apply(base3, 2, function(x)ifelse(x=="Sim",1,0 )) %>% as.data.frame()
-base3$quant_plant <- apply(base3, 1, sum)
+base$quant_plant <- apply(base3, 1, sum)
 
-quant <- cbind(base3$quant_plant,base2$quant_jogos)%>% as.data.frame()
+quant <- cbind(base$quant_plant,base$quant_jogos)%>% as.data.frame()
 colnames(quant) <- c("Plataforma","Genero")
 
 table(quant)
@@ -131,6 +173,21 @@ quant %>%
   ggplot(aes(x=Plataforma)) + 
   geom_bar(aes(fill=Genero), position = "dodge")+ylab("Frequência")+scale_fill_discrete(name="Gênero")
 
+#ESCOLARIDADE POR DINHEIRO GASTO
+
+base%>%ggplot(aes(x=escolaridade))+geom_bar(aes(fill=dinheiro), position="fill")
+
+tab1<-table(base$escolaridade,base$dinheiro)
+tab1
+
+chisq.test(tab1, correct=FALSE)
+
+#PREÇO JOGOs POR PRE VENDA
+base%>%ggplot(aes(x=p_venda))+geom_bar(aes(fill=preco_jogos), position="fill")
+
+#DINHEIRO POR PREÇO JOGOS
+
+base%>%ggplot(aes(x=dinheiro))+geom_bar(aes(fill=requisitos), position="fill")
 
 #Genero
 
@@ -138,23 +195,29 @@ barplot(prop.table(table(base$sexo)),ylim = c(0, 1),main="Genero")
 summary(base$sexo)
 
 ggplot(base, aes(x=sexo))+
-  geom_bar(fill="#00bfc4")+
+  geom_bar(fill=c("#f2766d", "#00bfc4", "#7cae00"))+
   theme_light()+
   theme(legend.position = "none")+
   xlab("Gênero")+ylab("Frequência")
 
+
+ggplot(base, aes(x=escolaridade))+
+  geom_bar(fill=c("#f2766d","#de8c00", "#b79f00","#7cae00", "#00ba38", "#00c08b", "#00bfc4"))+
+  theme_light()+
+  theme(legend.position = "none")+
+  xlab("Escolaridade")+ylab("Frequência")
  
 #Cat idade por tempo
 
-tab1_hora_cat <- table(base$idade.cat, base$horas_semana) %>% as.matrix(); tab1_hora_cat
+tab1_hora_cat <- table(base$sexo, base$horas_semana) %>% as.matrix(); tab1_hora_cat
 chisq.test(tab1_hora_cat,correct=FALSE)
 
 base$horas_semana=base$horas_semana%>%ordered(levels=c("menos de 2 horas","de 2 à 4 horas", "de 4 à 6 horas", "mais de 6 horas"))%>%as.factor()
 
 base %>%
-  ggplot(aes(x=idade.cat)) + 
+  ggplot(aes(x=sexo)) + 
   geom_bar(aes(fill=horas_semana), position = "fill") + 
-  ylab("Frequência Relativa")+theme_light()+xlab("Categoria de Idade")+
+  ylab("Frequência")+theme_light()+xlab("Gênero")+
   scale_fill_discrete(name="Horas Semanais", labels=c("Menos de 2 horas","De 2 à 4 horas", "De 4 à 6 horas", "Mais de 6 horas"))
   
 #Cat idade por preço
@@ -184,6 +247,9 @@ base %>%
 tab1_hora_cat <- table(base$midia, base$preco_jogos) %>% as.matrix(); tab1_hora_cat
 chisq.test(tab1_hora_cat,correct=FALSE)
 
+round((prop.table(table(base$escolaridade)))*100,1)
+10.21+89.45+0.33
+
 base %>%
   ggplot(aes(x=midia)) + 
   geom_bar(aes(fill=preco_jogos), position = "fill") + 
@@ -206,5 +272,3 @@ ggplot(base, aes(x=requisitos))+
   theme_light()+
   theme(legend.position = "none")+
   xlab("Já deixou de jogar por causa dos requisitos?")+ylab("Frequência")
-
-#
